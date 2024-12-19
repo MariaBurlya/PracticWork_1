b3d272f149546ca9642e03dab8dcdb619a4a24e7
Marrrrrusya <166085136+MariaBurlya@users.noreply.github.com>
2024-12-19 21:47:46 +0700

    Create Delete.cpp

---
+++ Delete.cpp
@@ -0,0 +1,7 @@
+#include "Delete.h"
+#include <iostream>
+
+void Delete::remove(const string& task) {
+    cout << "Выполнение запроса DELETE: " << task << endl;
+    cout << "Данные были удалены" << endl;
+}
