f3a829579bc7574547da720b062d902211fd5dc8
Marrrrrusya <166085136+MariaBurlya@users.noreply.github.com>
2024-12-19 21:48:54 +0700

    Create Insert.cpp

---
+++ Insert.cpp
@@ -0,0 +1,7 @@
+#include "Insert.h"
+#include <iostream>
+
+void Insert::insert(const string& task) {
+    cout << "Выполнение запроса INSERT: " << task << endl;
+    cout << "Данные были добавлены" << endl;
+}
